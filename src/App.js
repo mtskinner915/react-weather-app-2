ce586509f4c9b01cf3335848c7f07c3e89597569
mtskinner915 <margarettaylor915@gmail.com>
2022-07-05 14:50:29 -0700

    Added footer link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer className="App-link">
+        <a
+          className="Git-link"
+          href="https://github.com/mtskinner915/react-weather-app-2"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open-source code
+        </a>
+        <span> by Margaret Skinner</span>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
