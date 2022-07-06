83ddedd44daa55b8ed766d43523724a67ce09e5e
mtskinner915 <margarettaylor915@gmail.com>
2022-07-06 11:44:44 -0700

    Completed celsius to fahrenheit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -20,10 +21,7 @@ export default function WeatherInfo(props) {
                 alt={props.data.description}
               />
             </div>
-            <span className="Degree">
-              {Math.round(props.data.temperature)}{" "}
-            </span>
-            <span className="Units">°C | °F</span>
+            <WeatherTemperature celsius={props.data.temperature} />
           </div>
           <div className="col">
             <ul className="Weather-details">
