99090af00e9880b65d1262725c51f7243dbd165c
mtskinner915 <margarettaylor915@gmail.com>
2022-07-07 15:13:13 -0700

    Started creating forecast api integration

--- src/Weather.js
+++ src/Weather.js
@@ -9,6 +9,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
@@ -48,7 +49,7 @@ export default function Weather(props) {
           <input type="submit" value="Search" className="Button" />
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
